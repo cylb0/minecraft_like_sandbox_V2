924ce455f6416b9d1da9e33aa0e4af8d36e4df9d
Morgan <morgan.foucaut@greta-formations.fr>
2025-03-20 18:37:48 +0100

    test: Player spawn and camera

--- __tests__/units/Player.test.ts
+++ __tests__/units/Player.test.ts
@@ -1,9 +1,17 @@
+jest.mock("three", () => ({
+    ...jest.requireActual("three"),
+    WebGLRenderer: require("../../__mocks__/three/WebGLRenderer").default,
+}));
+jest.mock("@/world/World");
+
+import { DEFAULT_BLOCK_SIZE } from "@/constants/block";
 import { PLAYER_DIMENSIONS, PLAYER_SPAWN_POSITION } from "@/constants/player";
+import Camera from "@/core/scene/Camera";
+import { CameraMode } from "@/types/Camera";
 import Player from "@/units/Player";
 import World from "@/world/World";
 import { BoxGeometry, Mesh, PerspectiveCamera, Scene } from "three";
 
-jest.mock("@/world/World");
 jest.mock("three/examples/jsm/math/SimplexNoise");
 
 describe("Player class", () => {
@@ -17,6 +25,7 @@ describe("Player class", () => {
         scene = new Scene();
         camera = new PerspectiveCamera();
         world = new World(seed);
+        console.log(world)
         player = new Player(scene, camera, world);
     });
 
@@ -25,28 +34,40 @@ describe("Player class", () => {
     });
 
     it("should create a player mesh with correct dimension", () => {
-        const mesh = player.children[1] as Mesh;
+        const mesh = player.children.find(child => child instanceof Mesh);
         expect(mesh).toBeDefined();
-        expect(BoxGeometry).toHaveBeenCalledWith(PLAYER_DIMENSIONS.width, PLAYER_DIMENSIONS.height, PLAYER_DIMENSIONS.depth);
-        expect((mesh.geometry as any).parameters).toEqual({
-            width: PLAYER_DIMENSIONS.width,
-            height: PLAYER_DIMENSIONS.height,
-            depth: PLAYER_DIMENSIONS.depth,
-        })
+        expect(mesh!.geometry).toBeDefined();
 
-        const geometry = mesh.geometry as BoxGeometry;
+        expect(mesh!.geometry).toHaveProperty("parameters");
+
+        const geometry = mesh!.geometry as BoxGeometry;
+
         expect(geometry.parameters.width).toBe(PLAYER_DIMENSIONS.width);
         expect(geometry.parameters.height).toBe(PLAYER_DIMENSIONS.height);
         expect(geometry.parameters.depth).toBe(PLAYER_DIMENSIONS.depth);
     });
 
-    it("should position the player at spawn position", () => {
+    it("should attach the camera to the player", () => {
+        expect(player.children).toContain(camera);
+    });
+
+    it("should spawn at correct location", () => {
+        const expectedY = (PLAYER_DIMENSIONS.height / 2) + (DEFAULT_BLOCK_SIZE / 2);
+
         expect(player.position.x).toBe(PLAYER_SPAWN_POSITION.x);
-        expect(player.position.y).toBe(PLAYER_SPAWN_POSITION.y);
+        expect(player.position.y).toBe(expectedY);
         expect(player.position.z).toBe(PLAYER_SPAWN_POSITION.z);
     });
 
-    it("should add the camera to the player's object3D", () => {
-        expect(player.children).toContain(camera);
+    it("should update camera position for FPS mode", () => {
+        Object.defineProperty(Camera, "mode", { get: () => CameraMode.FPS });
+        player = new Player(scene, camera, world);
+        expect(camera.position.y).toBeCloseTo(PLAYER_DIMENSIONS.height * 3 / 4);
+    });
+
+    it("should update camera position for TPS mode", () => {
+        Object.defineProperty(Camera, "mode", { get: () => CameraMode.TPS });
+        player = new Player(scene, camera, world);
+        expect(camera.position).toEqual(Camera.tpsOffset);
     });
 });
