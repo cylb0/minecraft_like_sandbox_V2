454a624803c89989f971849a9ab8ae0e72a6643d
Morgan <morgan.foucaut@greta-formations.fr>
2025-03-20 13:46:33 +0100

    test: updated Camera tests

--- __tests__/core/scene/Camera.test.ts
+++ __tests__/core/scene/Camera.test.ts
@@ -1,21 +1,23 @@
+jest.mock("three");
+jest.mock("three/examples/jsm/controls/OrbitControls.js");
+
 import Camera from "@/core/scene/Camera"
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import Renderer from "@/core/scene/Renderer";
 import { Vector3, WebGLRenderer } from "three";
+import { CameraMode } from "@/types/Camera";
 
-jest.mock("three");
-jest.mock("three/examples/jsm/controls/OrbitControls.js");
 jest.mock("@/core/scene/Renderer");
 
 describe("Camera singleton", () => {
     test("should return the same instance of Camera", () => {
-        const camera1 = Camera.playerCamera;
-        const camera2 = Camera.playerCamera;
+        const camera1 = Camera.camera;
+        const camera2 = Camera.camera;
         expect(camera1).toBe(camera2);
     });
 
     test("should return a valid `THREE.PerspectiveCamera`", () => {
-        const camera = Camera.playerCamera;
+        const camera = Camera.camera;
         expect(camera).toEqual(expect.objectContaining({
             position: expect.objectContaining({
                 set: expect.any(Function)
@@ -23,32 +25,47 @@ describe("Camera singleton", () => {
         }));
     });
 
-    describe("addOrbitControls", () => {
-        let orbitControlsMock: jest.Mock;
+    describe("switchCamera", () => {
+        const positionMock = new Vector3(1, 2, 3);
 
         beforeEach(() => {
-            orbitControlsMock = OrbitControls as jest.Mock;
-            orbitControlsMock.mockClear();
             (Renderer as any).renderer = new WebGLRenderer();
         });
 
-        test("should create OrbitControls instance", () => {
-            Camera.addOrbitControls(new Vector3());
-            expect(orbitControlsMock).toHaveBeenCalledWith(Camera.orbitCamera, Renderer.renderer.domElement);
+        test("should create OrbitControls instance when switching to ORBIT mode", () => {
+            Camera.switchCamera(CameraMode.ORBIT, positionMock)
+            expect(Camera.controls).toBeDefined();
+            expect(OrbitControls).toHaveBeenCalledWith(Camera.camera, Renderer.renderer.domElement);
         });
 
-        test("should set the camera position", () => {
+        test("should dispose of OrbitControls when switching to another mode", () => {
+            Camera.switchCamera(CameraMode.ORBIT, positionMock);
+
+            expect(Camera.controls).toBeDefined();
+
+            const disposeSpy = jest.spyOn(Camera.controls!, "dispose");
+
+            Camera.switchCamera(CameraMode.PLAYER, positionMock);
+
+            expect(disposeSpy).toHaveBeenCalledTimes(1);
+            
+            disposeSpy.mockRestore();
+        });
+
+        test("should set the camera position when switching to ORBIT mode", () => {
             const position = new Vector3(2, 0, 2);
-            const setSpy = jest.spyOn(Camera.orbitCamera.position, "copy");
+            const copySpy = jest.spyOn(Camera.camera.position, "copy");
 
-            Camera.addOrbitControls(position);
+            Camera.switchCamera(CameraMode.ORBIT, position);
 
-            expect(setSpy).toHaveBeenCalledWith(position);
-            expect(Camera.orbitCamera.position).toEqual(expect.objectContaining({
+            expect(copySpy).toHaveBeenCalledWith(position);
+            expect(Camera.camera.position).toEqual(expect.objectContaining({
                 x: position.x,
                 y: position.y,
                 z: position.z,
             }));
+
+            copySpy.mockRestore();
         })
     });
 });
